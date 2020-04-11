3c31fd520a51bb72a8a4f199ce16d50baad1de76
Joao Victor Flores <j199818@dac.unicamp.br>
2020-04-11 02:59:23 -0300

    tab bar item

--- Calculator++/Calculator Notes/Controllers/Gallery/CollectionViewCell.swift
+++ Calculator++/Calculator Notes/Controllers/Gallery/CollectionViewCell.swift
@@ -1,16 +1,21 @@
 //
 //  CollectionViewCell.swift
-//  IOSDeleteItemsCollectionViewTutorial
+//  Calculator Notes
 //
-//  Created by Arthur Knopper on 25/04/2019.
-//  Copyright © 2019 Arthur Knopper. All rights reserved.
+//  Created by Joao Flores on 08/04/20.
+//  Copyright © 2020 Joao Flores. All rights reserved.
 //
 
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    //MARK: - PROPERTIES
+    
     @IBOutlet weak var checkmarkLabel: UILabel!
+    @IBOutlet weak var viewPhoto: UIView!
+    
+    @IBOutlet weak var imageCell: UIImageView!
     
     var isInEditingMode: Bool = false {
         didSet {
@@ -26,4 +31,11 @@ class CollectionViewCell: UICollectionViewCell {
         }
     }
     
+    func cropBounds(viewlayer: CALayer, cornerRadius: Float) {
+        
+        let imageLayer = viewlayer
+        imageLayer.cornerRadius = CGFloat(cornerRadius)
+        imageLayer.masksToBounds = true
+    }
 }
+
--- Calculator++/Calculator Notes/Controllers/Gallery/CollectionViewController.swift
+++ Calculator++/Calculator Notes/Controllers/Gallery/CollectionViewController.swift
@@ -1,19 +1,27 @@
 //
-//  CollectionViewController.swift
-//  IOSDeleteItemsCollectionViewTutorial
+//  CollectionViewCell.swift
+//  Calculator Notes
 //
-//  Created by Arthur Knopper on 24/04/2019.
-//  Copyright © 2019 Arthur Knopper. All rights reserved.
+//  Created by Joao Flores on 08/04/20.
+//  Copyright © 2020 Joao Flores. All rights reserved.
 //
 
 import UIKit
+import Photos
+import AssetsPickerViewController
+import DTPhotoViewerController
+import CoreData
 
 private let reuseIdentifier = "Cell"
 
-class CollectionViewController: UICollectionViewController {
-    @IBOutlet weak var deleteButton: UIBarButtonItem!
+class CollectionViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-    var modelData = ["One","Two","Three","Four","Five"]
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    var modelData = ModelController().fetchImageObjectsInit()
+    var image: UIImage!
+    var modelController = ModelController()
     
     @IBAction func deleteItem(_ sender: Any) {
         if let selectedCells = collectionView?.indexPathsForSelectedItems {
@@ -26,18 +34,29 @@ class CollectionViewController: UICollectionViewController {
         }
     }
     
-    @IBAction func teste(_ sender: Any) {
-        print("teste")
-        let name = "John"
-        modelData.append(name)
-        let indexPath = IndexPath(row: modelData.count - 1, section: 0)
-        collectionView?.insertItems(at: [indexPath])
+    @IBAction func addPhoto(_ sender: Any) {
+        openGalery()
+        
     }
     
+    //    MARK: - Take Profile Image
+    func openGalery() {
+        
+        let picker = AssetsPickerViewController()
+        picker.pickerDelegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    //    MARK: - Life cicle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.backgroundColor = UIColor.black
+        
         navigationItem.leftBarButtonItem = editButtonItem
+        
+//        populate()
     }
     
     override func setEditing(_ editing: Bool, animated: Bool) {
@@ -50,31 +69,35 @@ class CollectionViewController: UICollectionViewController {
             cell.isInEditingMode = editing
         }
     }
-
-   
-    // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return modelData.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
         
-        cell.titleLabel.text = modelData[indexPath.row]
+        //        cell.titleLabel.text = modelData[indexPath.row]
         cell.isInEditingMode = isEditing
-    
+        cell.cropBounds(viewlayer: cell.viewPhoto.layer, cornerRadius: 15)
+        cell.imageCell.image = modelData[indexPath[1]]
+        
         return cell
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if !isEditing {
             deleteButton.isEnabled = false
+            
+            let imageView = UIImageView(image: modelData[indexPath[1]])
+            let viewController = DTPhotoViewerController(referencedView: imageView, image: modelData[indexPath[1]])
+            self.present(viewController, animated: true, completion: nil)
         } else {
             deleteButton.isEnabled = true
         }
@@ -86,4 +109,92 @@ class CollectionViewController: UICollectionViewController {
         }
     }
     
+}
+
+extension CollectionViewController: AssetsPickerViewControllerDelegate {
+    
+    func assetsPickerCannotAccessPhotoLibrary(controller: AssetsPickerViewController) {}
+    
+    func assetsPickerDidCancel(controller: AssetsPickerViewController) {}
+    
+    func assetsPicker(controller: AssetsPickerViewController, selected assets: [PHAsset]) {
+        for asset in assets {
+            
+            image = getAssetThumbnail(asset: asset, size: 200)
+            let imageView = UIImageView(image: image!)
+            modelData.append(image)
+            
+            let indexPath = IndexPath(row: modelData.count - 1, section: 0)
+            collectionView!.insertItems(at: [indexPath])
+        
+            modelController.saveImageObject(image: image)
+            modelController.fetchImageObjects()
+            print(modelController.images.count)
+        }
+    }
+    
+    func getAssetThumbnail(asset: PHAsset, size: CGFloat) -> UIImage {
+        let retinaScale = UIScreen.main.scale
+        let retinaSquare = CGSize(width: size * retinaScale, height: size * retinaScale)
+        let cropSizeLength = min(asset.pixelWidth, asset.pixelHeight)
+        let square = CGRect(x: 0, y: 0, width: CGFloat(cropSizeLength), height: CGFloat(cropSizeLength))
+        let cropRect = square.applying(CGAffineTransform(scaleX: 1.0/CGFloat(asset.pixelWidth), y: 1.0/CGFloat(asset.pixelHeight)))
+        
+        let manager = PHImageManager.default()
+        let options = PHImageRequestOptions()
+        var thumbnail = UIImage()
+        
+        options.isSynchronous = true
+        options.deliveryMode = .highQualityFormat
+        options.resizeMode = .exact
+        options.normalizedCropRect = cropRect
+        
+        manager.requestImage(for: asset, targetSize: retinaSquare, contentMode: .aspectFit, options: options, resultHandler: {(result, info)->Void in
+            thumbnail = result!
+        })
+        return thumbnail
+    }
+    
+    func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
+        
+        let cgimage = image.cgImage!
+        let contextImage: UIImage = UIImage(cgImage: cgimage)
+        let contextSize: CGSize = contextImage.size
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        var cgwidth: CGFloat = CGFloat(width)
+        var cgheight: CGFloat = CGFloat(height)
+        
+        // See what size is longer and create the center off of that
+        if contextSize.width > contextSize.height {
+            posX = ((contextSize.width - contextSize.height) / 2)
+            posY = 0
+            cgwidth = contextSize.height
+            cgheight = contextSize.height
+        } else {
+            posX = 0
+            posY = ((contextSize.height - contextSize.width) / 2)
+            cgwidth = contextSize.width
+            cgheight = contextSize.width
+        }
+        
+        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+        
+        // Create bitmap image from context using the rect
+        let imageRef: CGImage = cgimage.cropping(to: rect)!
+        
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
+        
+        return image
+    }
+    
+    func assetsPicker(controller: AssetsPickerViewController, shouldSelect asset: PHAsset, at indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func assetsPicker(controller: AssetsPickerViewController, didSelect asset: PHAsset, at indexPath: IndexPath) {}
+    func assetsPicker(controller: AssetsPickerViewController, shouldDeselect asset: PHAsset, at indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func assetsPicker(controller: AssetsPickerViewController, didDeselect asset: PHAsset, at indexPath: IndexPath) {}
 }
