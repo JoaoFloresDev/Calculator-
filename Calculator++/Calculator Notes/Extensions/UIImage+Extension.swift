372bb77e161b7cdac7dbe161ebb43bc6211785a7
João Flores <j199818@dac.unicamp.br>
2022-12-02 19:51:07 -0300

    Refatoraçào

--- Calculator++/Calculator Notes/Extensions/UIImage+Extension.swift
+++ Calculator++/Calculator Notes/Extensions/UIImage+Extension.swift
@@ -14,3 +14,36 @@ extension UIImageView {
         self.image = UIImage(named: image.rawValue)
     }
 }
+
+extension UIImage {
+    func cropToBounds(width: Double, height: Double) -> UIImage {
+        
+        guard let cgimage = self.cgImage else { return self }
+        let contextImage: UIImage = UIImage(cgImage: cgimage)
+        let contextSize: CGSize = contextImage.size
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        var cgwidth: CGFloat = CGFloat(width)
+        var cgheight: CGFloat = CGFloat(height)
+        
+        if contextSize.width > contextSize.height {
+            posX = ((contextSize.width - contextSize.height) / 2)
+            posY = 0
+            cgwidth = contextSize.height
+            cgheight = contextSize.height
+        } else {
+            posX = 0
+            posY = ((contextSize.height - contextSize.width) / 2)
+            cgwidth = contextSize.width
+            cgheight = contextSize.width
+        }
+        
+        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+        
+        guard let imageRef: CGImage = cgimage.cropping(to: rect) else { return self }
+        
+        let image: UIImage = UIImage(cgImage: imageRef, scale: self.scale, orientation: self.imageOrientation)
+        
+        return image
+    }
+}
--- Calculator++/Calculator Notes/Scenes/Gallery/Controllers/CollectionViewController.swift
+++ Calculator++/Calculator Notes/Scenes/Gallery/Controllers/CollectionViewController.swift
@@ -25,8 +25,6 @@ import Foundation
 import AVFoundation
 import AVKit
 
-private let reuseIdentifier = "Cell"
-
 class CollectionViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, GADBannerViewDelegate, GADInterstitialDelegate {
     
     //    MARK: - Variables
@@ -36,6 +34,8 @@ class CollectionViewController: UICollectionViewController, UINavigationControll
     var modelController = ModelController()
     
     var bannerView: GADBannerView!
+    var interstitial: GADInterstitial!
+    var galleryService = GalleryService()
     
     //    MARK: - IBOutlet
     @IBOutlet weak var deleteButton: UIBarButtonItem!
@@ -63,7 +63,7 @@ class CollectionViewController: UICollectionViewController, UINavigationControll
     }
     
     @IBAction func deleteItem(_ sender: Any) {
-        ConfirmationReset()
+        deleteConfirmation()
     }
     
     @IBAction func addPhoto(_ sender: Any) {
@@ -80,30 +80,7 @@ class CollectionViewController: UICollectionViewController, UINavigationControll
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setup()
-        if #available(iOS 15, *) {
-            let appearance = UITabBarAppearance()
-            appearance.configureWithDefaultBackground()
-            appearance.stackedLayoutAppearance.normal.iconColor = .systemGray
-            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemGray]
-            
-            appearance.stackedLayoutAppearance.selected.iconColor = .systemBlue
-            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
-            tabBarController?.tabBar.standardAppearance = appearance
-            tabBarController?.tabBar.scrollEdgeAppearance = appearance
-        }
-        
-        if #available(iOS 13, *) {
-            let appearance = UITabBarAppearance()
-            appearance.shadowImage = UIImage()
-            appearance.shadowColor = .white
-            
-            appearance.stackedLayoutAppearance.normal.iconColor = .systemGray
-            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemGray]
-            appearance.stackedLayoutAppearance.selected.iconColor = .systemBlue
-            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
-            
-            tabBarController?.tabBar.standardAppearance = appearance
-        }
+        self.tabBarController?.setup()
         
         UserDefaults.standard.set(true, forKey:"InGallery")
         navigationItem.leftBarButtonItem =  editButtonItem
@@ -133,192 +110,11 @@ class CollectionViewController: UICollectionViewController, UINavigationControll
         collectionView?.collectionViewLayout = layout
     }
     
-    var interstitial: GADInterstitial!
-    
-    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
-        let getAddPhotoCounter = UserDefaultService().getAddPhotoCounter()
-        if getAddPhotoCounter > 5 {
-            interstitial.present(fromRootViewController: self)
-            UserDefaultService().setAddPhotoCounter(status: 0)
-        }
-    }
-    
-    func createAndLoadInterstitial() -> GADInterstitial {
-      let interstitial = GADInterstitial(adUnitID: "ca-app-pub-8858389345934911/8516660323")
-      interstitial.delegate = self
-      interstitial.load(GADRequest())
-      return interstitial
-    }
-
-    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
-      interstitial = createAndLoadInterstitial()
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         checkPurchase()
     }
     
-    //    MARK: - Ads
-    func checkPurchase() {
-        if(RazeFaceProducts.store.isProductPurchased("NoAds.Calc") || (UserDefaults.standard.object(forKey: "NoAds.Calc") != nil)) {
-            bannerView?.removeFromSuperview()
-        } else {
-            let getAddPhotoCounter = UserDefaultService().getAddPhotoCounter()
-            if getAddPhotoCounter > 5 {
-                let request = GADRequest()
-                interstitial = createAndLoadInterstitial()
-                interstitial.load(request)
-                interstitial.delegate = self
-            }
-        }
-    }
     
-    func setupAds() {
-        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = ["bc9b21ec199465e69782ace1e97f5b79"]
-        
-        bannerView = GADBannerView(adSize: kGADAdSizeLargeBanner)
-        addBannerViewToView(bannerView)
-        
-        bannerView.adUnitID = "ca-app-pub-8858389345934911/5265350806"
-        bannerView.rootViewController = self
-        
-        bannerView.load(GADRequest())
-        bannerView.delegate = self
-        checkPurchase()
-    }
-    
-    func addBannerViewToView(_ bannerView: GADBannerView) {
-        bannerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(bannerView)
-        view.addConstraints(
-            [NSLayoutConstraint(item: bannerView,
-                                attribute: .bottom,
-                                relatedBy: .equal,
-                                toItem: bottomLayoutGuide,
-                                attribute: .top,
-                                multiplier: 1,
-                                constant: 0),
-             NSLayoutConstraint(item: bannerView,
-                                attribute: .centerX,
-                                relatedBy: .equal,
-                                toItem: view,
-                                attribute: .centerX,
-                                multiplier: 1,
-                                constant: 0)
-            ])
-    }
-    
-    //    MARK: - Collection View
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        
-        if(editing) {
-            deleteButton.isEnabled = true
-            saveButton.isEnabled = true
-        } else {
-            deleteButton.isEnabled = false
-            saveButton.isEnabled = false
-            self.rateApp()
-        }
-        
-        collectionView?.allowsMultipleSelection = editing
-        if let indexPaths = collectionView?.indexPathsForVisibleItems {
-            for indexPath in indexPaths {
-                let cell = collectionView?.cellForItem(at: indexPath) as? CollectionViewCell
-                cell?.isInEditingMode = editing
-            }
-        }
-    }
-    
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let presentPlaceHolderImage = !modelData.isEmpty
-        placeHolderImage.isHidden = presentPlaceHolderImage
-        switch section {
-        case .zero:
-            return 1
-        default:
-            return modelData.count
-        }
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        switch indexPath.section {
-        case .zero:
-            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CollectionViewCell {
-                cell.imageCell.image = UIImage(named: "folder")
-                return cell
-            } else {
-                return collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-            }
-        default:
-            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CollectionViewCell {
-                cell.isInEditingMode = isEditing
-                if indexPath.indices.contains(1),
-                   modelData.indices.contains(indexPath[1]) {
-                cell.imageCell.image = cropToBounds(image: modelData[indexPath[1]], width: 200, height: 200)
-                }
-                applyshadowWithCorner(containerView : cell)
-
-                return cell
-            } else {
-                return collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-            }
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-            return CGSize(width: collectionView.frame.width, height: 0)
-    }
-    
-    func applyshadowWithCorner(containerView : UIView){
-        containerView.clipsToBounds = false
-        containerView.layer.shadowColor = UIColor.black.cgColor
-        containerView.layer.shadowOpacity = 0.5
-        containerView.layer.shadowOffset = CGSize.zero
-        containerView.layer.shadowRadius = 3
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        switch indexPath.section {
-        case .zero:
-            self.navigationController?.pushViewController(UIViewController(), animated: true)
-        default:
-            if !isEditing {
-                if indexPath.indices.contains(1),
-                   modelData.indices.contains(indexPath[1]) {
-                    self.presentImageGallery(GalleryViewController(startIndex: indexPath[1], itemsDataSource: self))
-                }
-            }
-        }
-    }
-    
-    func ConfirmationReset() {
-        let refreshAlert = UIAlertController(title: Text.deleteFiles.rawValue.localized(),
-                                             message: nil,
-                                             preferredStyle: UIAlertController.Style.alert)
-        
-        refreshAlert.modalPresentationStyle = .popover
-        
-        refreshAlert.addAction(UIAlertAction(title: Text.cancel.rawValue.localized(),
-                                             style: .destructive, handler: nil))
-        
-        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
-            if let selectedCells = self.collectionView?.indexPathsForSelectedItems {
-                let items = selectedCells.map { $0.item }.sorted().reversed()
-                for item in items {
-                    self.modelData.remove(at: item)
-                    self.modelController.deleteImageObject(imageIndex: item)
-                }
-                self.collectionView!.deleteItems(at: selectedCells)
-            }
-        }))
-        
-        present(refreshAlert, animated: true, completion: nil)
-    }
     
     //    MARK: - StoreKit
     func rateApp() {
@@ -333,56 +129,13 @@ extension CollectionViewController: AssetsPickerViewControllerDelegate {
     func assetsPicker(controller: AssetsPickerViewController, selected assets: [PHAsset]) {
         for asset in assets {
             if(asset.mediaType.rawValue != 2) {
-                image = getAssetThumbnail(asset: asset)
+                image = galleryService.getAssetThumbnail(asset: asset)
                 modelData.append(image)
                 let indexPath = IndexPath(row: modelData.count - 1, section: 0)
                 collectionView!.insertItems(at: [indexPath])
                 modelController.saveImageObject(image: image)
             }
         }
-    }
-    
-    
-    func getAssetThumbnail(asset: PHAsset) -> UIImage {
-        let manager = PHImageManager.default()
-        let option = PHImageRequestOptions()
-        var thumbnail = UIImage()
-        option.isSynchronous = true
-        manager.requestImage(for: asset, targetSize: CGSize(width: 1500, height: 1500), contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
-            thumbnail = result ?? UIImage()
-        })
-        return thumbnail
-    }
-    
-    func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
-        
-        guard let cgimage = image.cgImage else { return image }
-        let contextImage: UIImage = UIImage(cgImage: cgimage)
-        let contextSize: CGSize = contextImage.size
-        var posX: CGFloat = 0.0
-        var posY: CGFloat = 0.0
-        var cgwidth: CGFloat = CGFloat(width)
-        var cgheight: CGFloat = CGFloat(height)
-        
-        if contextSize.width > contextSize.height {
-            posX = ((contextSize.width - contextSize.height) / 2)
-            posY = 0
-            cgwidth = contextSize.height
-            cgheight = contextSize.height
-        } else {
-            posX = 0
-            posY = ((contextSize.height - contextSize.width) / 2)
-            cgwidth = contextSize.width
-            cgheight = contextSize.width
-        }
-        
-        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
-        
-        guard let imageRef: CGImage = cgimage.cropping(to: rect) else { return image }
-        
-        let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
-        
-        return image
     }
     
     func assetsPicker(controller: AssetsPickerViewController, shouldSelect asset: PHAsset, at indexPath: IndexPath) -> Bool {
