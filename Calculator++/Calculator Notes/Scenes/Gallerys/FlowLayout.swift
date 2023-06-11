ea0632cea2989f439c8280856a4c96a339435032
João Flores <j199818@dac.unicamp.br>
2023-06-11 03:17:25 -0300

    ultimo ajuste, as imagens não ficam embaixo da propaganda

--- Calculator++/Calculator Notes/Scenes/Gallerys/FlowLayout.swift
+++ Calculator++/Calculator Notes/Scenes/Gallerys/FlowLayout.swift
@@ -16,9 +16,49 @@ class FlowLayout: UICollectionViewFlowLayout {
         itemSize = CGSize(width: screenWidth/4, height: screenWidth/4)
         minimumInteritemSpacing = 20
         minimumLineSpacing = 20
+//        footerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 100)
+//        headerReferenceSize = CGSize.zero
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var layoutAttributes = super.layoutAttributesForElements(in: rect)
+        let footerAttributes = layoutAttributesForSupplementaryView(ofKind: UICollectionElementKindSectionFooter, at: IndexPath(item: 0, section: collectionView!.numberOfSections - 1))
+
+        if let footerAttributes = footerAttributes {
+            layoutAttributes?.append(footerAttributes)
+        }
+
+        return layoutAttributes
+    }
+
+    override func layoutAttributesForSupplementaryView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let attributes = super.layoutAttributesForSupplementaryView(ofKind: elementKind, at: indexPath)
+
+        if elementKind == UICollectionElementKindSectionFooter && indexPath.section == collectionView!.numberOfSections - 1 {
+            let footerAttributes = UICollectionViewLayoutAttributes(forSupplementaryViewOfKind: elementKind, with: indexPath)
+            let contentWidth = collectionViewContentSize.width - sectionInset.left - sectionInset.right
+            let footerHeight: CGFloat = 100
+
+            footerAttributes.frame = CGRect(x: sectionInset.left, y: attributes?.frame.maxY ?? 0, width: contentWidth, height: footerHeight)
+
+            return footerAttributes
+        }
+
+        return attributes
+    }
+
+    override var collectionViewContentSize: CGSize {
+        var contentSize = super.collectionViewContentSize
+        let lastSectionFooterHeight: CGFloat = 100
+
+        if collectionView!.numberOfSections > 0 {
+            contentSize.height += lastSectionFooterHeight
+        }
+
+        return contentSize
     }
 }
