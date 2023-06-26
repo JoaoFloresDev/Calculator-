d08ae93878bfcdd9e74af879d5df46d5236cf481
João Flores <j199818@dac.unicamp.br>
2023-06-26 11:29:30 -0300

    äa

---
+++ Calculator++/Calculator Notes/Scenes/Gallerys/BasicCollectionViewController.swift
@@ -0,0 +1,95 @@
+//
+//  BasicCollectionViewController.swift
+//  Calculator Notes
+//
+//  Created by Joao Victor Flores da Costa on 10/06/23.
+//  Copyright © 2023 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class BasicCollectionViewController: UICollectionViewController {
+    let deepSeparatorPath = "@"
+    let reuseIdentifier = "Cell"
+    let folderReuseIdentifier = "FolderCell"
+    var adsHandler: AdsHandler = AdsHandler()
+    let defaults = UserDefaults.standard
+    public var basePath = "@"
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    var image: UIImage?
+    var editLeftBarButtonItem: EditLeftBarButtonItem?
+    var additionsRightBarButtonItem: AdditionsRightBarButtonItem?
+    var foldersService = FoldersService(type: .image)
+
+    var navigationTitle: String?
+    
+    var filesIsExpanded = false {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView?.reloadSections(IndexSet(integer: 1))
+            }
+        }
+    }
+    
+    typealias BarButtonItemDelegate = AdditionsRightBarButtonItemDelegate & EditLeftBarButtonItemDelegate
+    
+    func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func setupCollectionViewLayout() {
+        let screenWidth = self.view.frame.size.width - 100
+        let flowLayout = FlowLayout(screenWidth: screenWidth)
+        if basePath == "@" {
+            flowLayout.headerReferenceSize = CGSize(width: screenWidth, height: 25)
+        }
+        collectionView?.collectionViewLayout = flowLayout
+    }
+    
+    func setupNavigationItems(delegate: BarButtonItemDelegate) {
+        self.navigationController?.setup()
+        self.tabBarController?.setup()
+        additionsRightBarButtonItem = AdditionsRightBarButtonItem(delegate: delegate)
+        navigationItem.rightBarButtonItem = additionsRightBarButtonItem
+        editLeftBarButtonItem = EditLeftBarButtonItem(basePath: basePath, delegate: delegate)
+        navigationItem.leftBarButtonItem = editLeftBarButtonItem
+    }
+    
+    // Collection View
+    func setEditionMode(_ editing: Bool) {
+        editLeftBarButtonItem?.setEditing(editing)
+        
+        collectionView?.allowsMultipleSelection = true
+        
+        if let indexPaths = collectionView?.indexPathsForVisibleItems {
+            for indexPath in indexPaths {
+                if let cell = collectionView?.cellForItem(at: indexPath) as? CollectionViewCell {
+                    cell.isInEditingMode = editing
+                }
+                if let cell = collectionView?.cellForItem(at: indexPath) as? FolderCollectionViewCell {
+                    cell.isInEditingMode = editing
+                }
+            }
+        }
+    }
+    
+    func commonViewDidLoad() {
+        setupCollectionViewLayout()
+        
+        collectionView?.register(HeaderView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "headerView")
+        collectionView?.register(FooterView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "footerView")
+        
+        if basePath != "@" {
+            filesIsExpanded = true
+        }
+    }
+}
+
+extension BasicCollectionViewController: HeaderViewDelegate {
+    func headerTapped(header: HeaderView) {
+        filesIsExpanded.toggle()
+        collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+}
+
