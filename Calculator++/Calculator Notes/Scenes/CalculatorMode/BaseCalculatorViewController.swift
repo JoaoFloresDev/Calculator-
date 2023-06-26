68bbcbd0758358e10b89ff0a1bfdee5c4ef536e8
João Flores <j199818@dac.unicamp.br>
2023-06-26 10:53:53 -0300

    äa

---
+++ Calculator++/Calculator Notes/Scenes/CalculatorMode/BaseCalculatorViewController.swift
@@ -0,0 +1,80 @@
+//
+//  BaseCalculatorViewController.swift
+//  Calculator Notes
+//
+//  Created by Joao Victor Flores da Costa on 14/08/22.
+//  Copyright © 2022 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseCalculatorViewController: UIViewController {
+    
+    // MARK: - Public Vars
+    var keyTemp = ""
+    var captureKey = 0
+    var runningNumber = ""
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
+    var currentOperation: Operation = .NULL
+    var key = UserDefaults.standard.string(forKey: "Key") ?? ""
+    let recoveryKey = "314159"
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    // MARK: - Operations
+    func clear() {
+        
+        runningNumber = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .NULL
+        outputLbl.text = "0 "
+    }
+    
+    func operation(operation: Operation) {
+        if currentOperation != .NULL {
+            if runningNumber != "" {
+                rightValue = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == .Add {
+                    result = "\((Double(leftValue) ?? Double(0)) + (Double(rightValue) ?? Double(0)))"
+                } else if currentOperation == .Subtract {
+                    result = "\((Double(leftValue) ?? Double(0)) - (Double(rightValue) ?? Double(0)))"
+                } else if currentOperation == .Multiply {
+                    result = "\((Double(leftValue) ?? Double(0)) * (Double(rightValue) ?? Double(0)))"
+                } else if currentOperation == .Divide {
+                    result = "\((Double(leftValue) ?? Double(0)) / (Double(rightValue) ?? Double(0)))"
+                }
+                
+                leftValue = result
+                if ((Double(result) ?? Double(0)).truncatingRemainder(dividingBy: 1) == 0) {
+                    result = "\((Int(Double(result) ?? Double(0))))"
+                }
+                outputLbl.text = result
+            }
+            currentOperation = operation
+        } else {
+            leftValue = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
+    
+    func dotPressed() {
+        if runningNumber.count <= 8 {
+            runningNumber += "."
+            outputLbl.text = runningNumber
+        }
+    }
+    
+    //    MARK: - Style
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
