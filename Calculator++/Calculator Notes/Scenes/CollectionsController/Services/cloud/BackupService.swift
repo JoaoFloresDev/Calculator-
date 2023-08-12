bc91f95f9e1ae67d251725c6b200f09d1543fd57
João Flores <j199818@dac.unicamp.br>
2023-08-12 15:56:49 -0300

    Refatorações

---
+++ Calculator++/Calculator Notes/Scenes/CollectionsController/Services/cloud/BackupService.swift
@@ -0,0 +1,80 @@
+import UIKit
+
+struct BackupService {
+    static func updateBackup() {
+        let photos = ModelController.listAllPhotos()
+        addIfNeed(photos: photos)
+        CloudKitImageService.deleteImages(names: NameManager.getNames())
+    }
+    
+    static func addIfNeed(photos: [Photo]) {
+        for photo in photos {
+            CloudKitImageService.imageExists(withName: photo.name) { exists, error in
+                if let error = error {
+                    print("\(Notifications.errorVerifyingImage) \(error.localizedDescription)")
+                    return
+                }
+                if exists {
+                    print(Notifications.imageExists)
+                } else {
+                    CloudKitImageService.saveImage(name: photo.name, image: photo.image) { success, error in
+                        if let error = error {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+        }
+        print(Notifications.success)
+    }
+    
+    static func hasDataInCloudKit(completion: @escaping (Bool, Error?, [(String, UIImage)]?) -> Void) {
+        CloudKitImageService.fetchImages { items, error in
+            if let error = error {
+                completion(false, error, nil)
+            } else {
+                if let items = items, !items.isEmpty {
+                    completion(true, nil, items)
+                } else {
+                    completion(false, nil, nil)
+                }
+            }
+        }
+    }
+    
+    static func restoreBackup(photos: [(String, UIImage)],
+                              completion: @escaping (Bool, Error?) -> Void) {
+        for photo in photos {
+            ModelController.saveImageObject(image: photo.1, path: photo.0)
+        }
+        for photo in photos {
+            if photo.0.filter({ $0 == "@" }).count > 1 {
+                var outputArray = convertStringToArray(input: photo.0)
+                outputArray.removeLast()
+                var foldersService = FoldersService(type: .image)
+                for index in 0..<outputArray.count {
+                    let basePath = "@" + concatenateStringsUpToIndex(array: outputArray, index: index)
+                    if !foldersService.checkAlreadyExist(folder: outputArray[index], basePath: basePath) {
+                        foldersService.add(folder: outputArray[index], basePath: basePath)
+                    }
+                }
+            }
+        }
+        
+        completion(true, nil)
+    }
+    
+    static func concatenateStringsUpToIndex(array: [String], index: Int) -> String {
+        var result = ""
+        for i in 0..<index {
+            result += array[i]
+        }
+        return result
+    }
+    
+    static func convertStringToArray(input: String) -> [String] {
+        let components = input.components(separatedBy: "@")
+        let filteredComponents = components.filter { !$0.isEmpty }
+        return filteredComponents
+    }
+}
--- Calculator++/Calculator Notes/Scenes/CollectionsController/Services/cloud/CloudKitService.swift
+++ Calculator++/Calculator Notes/Scenes/CollectionsController/Services/cloud/CloudKitService.swift
@@ -1,27 +1,15 @@
 import CloudKit
 import UIKit
 
-struct Notifications {
-    static let itemSaved = "Item saved"
-    static let errorSavingItem = "Error saving item:"
-    static let errorFetchingItems = "Error fetching items:"
-    static let errorDeletingItem = "Error deleting item:"
-    static let itemDeleted = "Item deleted:"
-    static let userExists = "Usuário com o nome já existe."
-    static let success = "sucesso!"
-    static let errorVerifyingUser = "Erro ao verificar existência do usuário:"
-    static let itemsDeleted = "Items deleted:"
-}
-
-class CloudKitService: ObservableObject {
-    private static let dataBase = CKContainer(identifier: "iCloud.calculatorNotes").publicCloudDatabase
+class CloudKitImageService: ObservableObject {
+    private static let database = CKContainer(identifier: "iCloud.calculatorNotes").publicCloudDatabase
     
-    static var items: [(String, UIImage)] = []
+    static var images: [(String, UIImage)] = []
     
-    static let recordTypeID = "ItemItem"
+    static let recordTypeIdentifier = "ImageItem"
     struct RecordKeys {
         static let name = "name"
-        static let userImage = "userImage"
+        static let image = "image"
     }
     
     struct PredicateFormats {
@@ -29,20 +17,20 @@ class CloudKitService: ObservableObject {
         static let alwaysTrue = NSPredicate(value: true)
     }
     
-    static func saveItem(name: String, userImage: UIImage, completion: @escaping (Bool, Error?) -> Void) {
-        let record = CKRecord(recordType: recordTypeID)
+    static func saveImage(name: String, image: UIImage, completion: @escaping (Bool, Error?) -> Void) {
+        let record = CKRecord(recordType: recordTypeIdentifier)
         record.setValue(name, forKey: RecordKeys.name)
         
-        if let imageData = UIImageJPEGRepresentation(userImage, 0.8) {
+        if let imageData = UIImageJPEGRepresentation(image, 0.8) {
             let tempDirectory = FileManager.default.temporaryDirectory
             let imageFileURL = tempDirectory.appendingPathComponent(UUID().uuidString + ".jpg")
             try? imageData.write(to: imageFileURL)
             
             let imageAsset = CKAsset(fileURL: imageFileURL)
-            record.setValue(imageAsset, forKey: RecordKeys.userImage)
+            record.setValue(imageAsset, forKey: RecordKeys.image)
         }
         
-        dataBase.save(record) { record, error in
+        database.save(record) { record, error in
             if record != nil, error == nil {
                 print(Notifications.itemSaved)
                 completion(true, nil)
@@ -53,10 +41,10 @@ class CloudKitService: ObservableObject {
         }
     }
     
-    static func fetchItems(completion: @escaping ([(String, UIImage)]?, Error?) -> Void) {
-        let query = CKQuery(recordType: recordTypeID, predicate: PredicateFormats.alwaysTrue)
+    static func fetchImages(completion: @escaping ([(String, UIImage)]?, Error?) -> Void) {
+        let query = CKQuery(recordType: recordTypeIdentifier, predicate: PredicateFormats.alwaysTrue)
         
-        dataBase.perform(query, inZoneWith: nil) { records, error in
+        database.perform(query, inZoneWith: nil) { records, error in
             if let error = error {
                 print("\(Notifications.errorFetchingItems) \(error.localizedDescription)")
                 completion(nil, error)
@@ -64,30 +52,30 @@ class CloudKitService: ObservableObject {
             }
             
             if let records = records {
-                var fetchedItems = [(String, UIImage)]()
+                var fetchedImages = [(String, UIImage)]()
                 
                 for record in records {
-                    if let itemName = record[RecordKeys.name] as? String,
-                       let imageAsset = record[RecordKeys.userImage] as? CKAsset,
+                    if let imageName = record[RecordKeys.name] as? String,
+                       let imageAsset = record[RecordKeys.image] as? CKAsset,
                        let imageData = try? Data(contentsOf: imageAsset.fileURL),
-                       let userImage = UIImage(data: imageData) {
-                        fetchedItems.append((itemName, userImage))
+                       let image = UIImage(data: imageData) {
+                        fetchedImages.append((imageName, image))
                     }
                 }
                 
                 DispatchQueue.main.async {
-                    items = fetchedItems
-                    print("! \(fetchedItems) !")
-                    completion(fetchedItems, nil)
+                    images = fetchedImages
+                    print("! \(fetchedImages) !")
+                    completion(fetchedImages, nil)
                 }
             }
         }
     }
     
-    static func deleteItem(name: String, completion: @escaping (Bool, Error?) -> Void) {
-        let query = CKQuery(recordType: recordTypeID, predicate: NSPredicate(format: PredicateFormats.nameEqual, name))
+    static func deleteImage(name: String, completion: @escaping (Bool, Error?) -> Void) {
+        let query = CKQuery(recordType: recordTypeIdentifier, predicate: NSPredicate(format: PredicateFormats.nameEqual, name))
         
-        dataBase.perform(query, inZoneWith: nil) { records, error in
+        database.perform(query, inZoneWith: nil) { records, error in
             if let error = error {
                 print("\(Notifications.errorFetchingItems) \(error.localizedDescription)")
                 completion(false, error)
@@ -95,7 +83,7 @@ class CloudKitService: ObservableObject {
             }
             
             if let recordToDelete = records?.first {
-                dataBase.delete(withRecordID: recordToDelete.recordID) { recordID, error in
+                database.delete(withRecordID: recordToDelete.recordID) { recordID, error in
                     if let error = error {
                         print("\(Notifications.errorDeletingItem) \(error.localizedDescription)")
                         completion(false, error)
@@ -108,10 +96,10 @@ class CloudKitService: ObservableObject {
         }
     }
     
-    static func userExists(withName name: String, completion: @escaping (Bool, Error?) -> Void) {
-        let query = CKQuery(recordType: recordTypeID, predicate: NSPredicate(format: PredicateFormats.nameEqual, name))
+    static func imageExists(withName name: String, completion: @escaping (Bool, Error?) -> Void) {
+        let query = CKQuery(recordType: recordTypeIdentifier, predicate: NSPredicate(format: PredicateFormats.nameEqual, name))
         
-        dataBase.perform(query, inZoneWith: nil) { records, error in
+        database.perform(query, inZoneWith: nil) { records, error in
             if let error = error {
                 print("\(Notifications.errorFetchingItems) \(error.localizedDescription)")
                 completion(false, error)
@@ -126,9 +114,9 @@ class CloudKitService: ObservableObject {
         }
     }
     
-    static func deletePhotos(nameList: [String]) {
-        for name in nameList {
-            deleteItem(name: name) { success, error in
+    static func deleteImages(names: [String]) {
+        for name in names {
+            deleteImage(name: name) { success, error in
                 if success && error == nil {
                     NameManager.deleteName(name)
                 }
@@ -137,9 +125,9 @@ class CloudKitService: ObservableObject {
     }
     
     static func deleteAllItems(completion: @escaping (Bool, Error?) -> Void) {
-        let query = CKQuery(recordType: recordTypeID, predicate: PredicateFormats.alwaysTrue)
+        let query = CKQuery(recordType: recordTypeIdentifier, predicate: PredicateFormats.alwaysTrue)
         
-        dataBase.perform(query, inZoneWith: nil) { records, error in
+        database.perform(query, inZoneWith: nil) { records, error in
             if let error = error {
                 print("\(Notifications.errorFetchingItems) \(error.localizedDescription)")
                 completion(false, error)
@@ -155,11 +143,11 @@ class CloudKitService: ObservableObject {
                         print("\(Notifications.errorDeletingItem) \(error.localizedDescription)")
                         completion(false, error)
                     } else {
-                        print(Notifications.itemsDeleted)
+                        print(Notifications.imagesDeleted)
                         completion(true, nil)
                     }
                 }
-                dataBase.add(operation)
+                database.add(operation)
             }
         }
     }
