7a48cd75dc9e93d8e44ee3a5d20e1b45d8533848
João Flores <j199818@dac.unicamp.br>
2023-08-24 11:29:46 -0300

    resolução de problema de path

---
+++ Calculator++/Calculator Notes/Scenes/CollectionsController/Services/cloud/CloudNotifications.swift
@@ -0,0 +1,11 @@
+struct Notifications {
+    static let itemSaved = "Item saved"
+    static let errorSavingItem = "Error saving item:"
+    static let errorFetchingItems = "Error fetching items:"
+    static let errorDeletingItem = "Error deleting item:"
+    static let itemDeleted = "Item deleted:"
+    static let imageExists = "Image with the name already exists."
+    static let success = "Success!"
+    static let errorVerifyingImage = "Error verifying image existence:"
+    static let imagesDeleted = "Images deleted:"
+}
