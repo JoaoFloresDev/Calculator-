a85a26924c841c7421beac9c898f5279e1134c3b
João Flores <j199818@dac.unicamp.br>
2023-08-28 21:10:41 -0300

    subi pro test fling

--- Calculator++/Calculator Notes/Scenes/CollectionsController/Services/ads/AdsHandler.swift
+++ Calculator++/Calculator Notes/Scenes/CollectionsController/Services/ads/AdsHandler.swift
@@ -1,39 +1,3 @@
-//import UIKit
-//import GoogleMobileAds
-//
-//class AdsHandler {
-//    var interstitial: GADInterstitial? = nil
-//    let adsService = AdsService()
-//
-//    func setupAds(controller: UIViewController,
-//                          bannerDelegate: GADBannerViewDelegate,
-//                          interstitialDelegate: GADInterstitialDelegate) {
-//        interstitial = adsService.createAndLoadInterstitial(delegate: interstitialDelegate)
-//        guard var interstitial = interstitial else {
-//            return
-//        }
-//        adsService.setupAds(controller: controller,
-//                            interstitial: &interstitial,
-//                            bannerDelegate: bannerDelegate,
-//                            interstitialDelegate: interstitialDelegate)
-//    }
-//
-//    func interstitialDidDismissScreen(delegate: GADInterstitialDelegate) {
-//        let adsService = AdsService()
-//        interstitial = adsService.createAndLoadInterstitial(delegate: delegate)
-//    }
-//
-//    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
-//        let getAddPhotoCounter = Defaults.getInt(.launchCounter)
-//        if getAddPhotoCounter > 5 {
-//            if let rootViewController = UIApplication.shared.keyWindow?.rootViewController {
-//                ad.present(fromRootViewController: rootViewController)
-//                Defaults.setInt(.launchCounter, 0)
-//            }
-//        }
-//    }
-//}
-
 import UIKit
 import GoogleMobileAds
 
