cb8e2195f5debdb3fcc0eae50fd6f38b774e05d1
joaofloresdev <j199818@dac.unicamp.br>
2023-09-09 18:24:34 -0300

    Separei os files

---
+++ Calculator++/Calculator Notes/Services/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Calculator Notes
+//
+//  Created by Joao Victor Flores da Costa on 09/09/23.
+//  Copyright © 2023 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let deepSeparatorPath = "@"
+    static let recoverPassword = "314159"
+}
