8dcf75ba8ac7fd43009fb5c065e4b2dbb82034ff
joaofloresdev <j199818@dac.unicamp.br>
2024-07-31 15:19:01 -0300

    Refatorando backup modal

--- Calculator++/Calculator Notes/Scenes/Backup/ChangeIconViewController.swift
+++ Calculator++/Calculator Notes/Scenes/Backup/ChangeIconViewController.swift
@@ -26,229 +26,14 @@ protocol BackupModalViewControllerDelegate {
 class BackupModalViewController: UIViewController {
     var delegate: BackupModalViewControllerDelegate?
     
-    lazy var modalTitleView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 16
-        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
-        let titleLabel = UILabel()
-        titleLabel.text = Text.backupSettings.localized()
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 14)
-        titleLabel.textColor = .black
-        
-        view.addSubview(titleLabel)
-        
-        titleLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-        
-        return view
-    }()
+    lazy var backupHeaderView = BackupHeaderView()
+    lazy var loginView = BackupLoginView(controller: self)
     
-    lazy var modalSubtitleView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 16
-        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
-        let titleLabel = UILabel()
-        titleLabel.text = Text.backupNavigationSubtitle.localized()
-        titleLabel.font = UIFont.systemFont(ofSize: 14)
-        titleLabel.textColor = .lightGray
-        titleLabel.numberOfLines = 0
-        titleLabel.textAlignment = .center
-        
-        view.addSubview(titleLabel)
-        
-        titleLabel.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        return view
-    }()
-    
-    
-    lazy var loginTitle: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 16
-        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
-        let titleLabel = UILabel()
-        titleLabel.text = "Já possui uma conta?"
-        titleLabel.font = UIFont.systemFont(ofSize: 14)
-        titleLabel.textColor = .darkGray
-        titleLabel.numberOfLines = 0
-        titleLabel.textAlignment = .center
-        
-        view.addSubview(titleLabel)
-        
-        titleLabel.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        return view
-    }()
-    
-    lazy var loginButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Login With Google", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .systemGray6
-        
-        let googleLogo = UIImage(named: "google_logo")
-        button.setImage(googleLogo, for: .normal)
-        
-        button.imageView?.contentMode = .scaleAspectFit
-        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
-        
-        button.layer.cornerRadius = 25
-        button.layer.masksToBounds = true
-        
-        button.snp.makeConstraints { make in
-            make.height.equalTo(50)
-        }
-        
-        button.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    @objc func loginTapped() {
-        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
-            guard let result = signInResult else {
-                if (error as? NSError)?.code == -5 {
-                    print("User cancelled")
-                } else {
-                    Alerts.showAlert(title: "Error", text: "\(error?.localizedDescription)\n\nSe você ainda não possui uma conta, selecione 'Sign up with google'" ?? "Something went wrong", controller: self)
-                }
-                return
-            }
-            
-            if let googleID = result.user.userID, let email = result.user.profile?.email {
-                self.manager.signInWithEmail(withEmail: email, password: googleID) { error in
-                    if let error {
-                        Alerts.showAlert(title: "Error", text: "\(error.localizedDescription)\n\nSe Você ainda não possui uma conta, selecione 'Sign up with google'", controller: self)
-                    } else {
-                        self.navigationController?.pushViewController(OnboardingAddPhotosViewController(), animated: true)
-                    }
-                }
-            } else {
-                Alerts.showAlert(title: "Error", text: "Something went wrong.", controller: self)
-            }
-        }
-    }
-    
-    let manager = AuthManager()
-
-    lazy var createAccountTitle: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 16
-        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
-        let titleLabel = UILabel()
-        titleLabel.text = "Ainda não? Crie agora!"
-        titleLabel.font = UIFont.systemFont(ofSize: 14)
-        titleLabel.textColor = .darkGray
-        titleLabel.numberOfLines = 0
-        titleLabel.textAlignment = .center
-        
-        view.addSubview(titleLabel)
-        
-        titleLabel.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        return view
-    }()
-    
-    lazy var createAccountButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Sign Up With Google", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .systemGray6
-        
-        let googleLogo = UIImage(named: "google_logo")
-        button.setImage(googleLogo, for: .normal)
-        
-        button.imageView?.contentMode = .scaleAspectFit
-        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
-        
-        button.layer.cornerRadius = 25
-        button.layer.masksToBounds = true
-        
-        button.snp.makeConstraints { make in
-            make.height.equalTo(50)
-        }
-        
-        button.addTarget(self, action: #selector(createAccountTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    @objc func createAccountTapped() {
-        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
-            guard let result = signInResult else {
-                if (error as? NSError)?.code == -5 {
-                    print("User cancelled")
-                } else {
-                    Alerts.showAlert(title: "Error", text: error?.localizedDescription ?? "Something went wrong", controller: self)
-                }
-                return
-            }
-            
-            if let googleID = result.user.userID, let email = result.user.profile?.email {
-                self.manager.createAccount(withEmail: email, password: googleID) { error in
-                    if let error {
-                        Alerts.showAlert(title: "Error", text: error.localizedDescription, controller: self)
-                    } else {
-                        self.navigationController?.pushViewController(OnboardingAddPhotosViewController(), animated: true)
-                    }
-                }
-            } else {
-                Alerts.showAlert(title: "Error", text: "Something went wrong.", controller: self)
-            }
-        }
-    }
-    
-    lazy var switchControl: UISwitch = {
-        let switchControl = UISwitch()
-        switchControl.addTarget(self, action: #selector(switchValueChanged(_:)), for: .valueChanged)
-        return switchControl
-    }()
-    
-    lazy var backupStatus: UIView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.spacing = 8
-        
-        let leftLabel = UILabel()
-        leftLabel.text = Text.backupStatus.localized()
-        leftLabel.font = UIFont.systemFont(ofSize: 14)
-        
-        stackView.addArrangedSubview(leftLabel)
-        stackView.addArrangedSubview(switchControl)
-        
-        let backupStatusView = UIView()
-        backupStatusView.backgroundColor = .systemGray5
-        backupStatusView.addSubview(stackView)
-        
-        stackView.snp.makeConstraints { make in
-            make.top.bottom.trailing.equalToSuperview().inset(8)
-            make.leading.equalToSuperview().inset(16)
-        }
-        
-        backupStatusView.snp.makeConstraints { make in
-            make.height.equalTo(50)
-        }
-        
-        return backupStatusView
-    }()
+    lazy var backupStatusView = BackupStatusView(delegate: delegate)
 
     lazy var restoreBackup: UIView = {
         let label = UILabel()
-        label.text = "Restaurar backup"//Text.restoreBackup.localized()
+        label.text = "Restaurar backup"
         label.font = UIFont.systemFont(ofSize: 14)
         let restoreBackupView = UIView()
         restoreBackupView.backgroundColor = .systemGray5
@@ -260,10 +45,9 @@ class BackupModalViewController: UIViewController {
         }
         
         restoreBackupView.snp.makeConstraints { make in
-            make.height.equalTo(50) // Definindo a altura desejada
+            make.height.equalTo(50)
         }
         
-        // Adicionar o gesture recognizer para tornar a view clicável
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.restoreBackupTapped))
         restoreBackupView.addGestureRecognizer(tapGesture)
         
@@ -294,13 +78,11 @@ class BackupModalViewController: UIViewController {
     }()
     
     @objc func updateBackupTapped() {
-//        isConnectedToWiFi { isConnected in
             guard Defaults.getBool(.iCloudEnabled) else {
                 Alerts.showBackupDisabled(controller: self)
                 return
             }
             
-//            if isConnected {
                 self.loadingAlert.startLoading {
                     FirebaseBackupService.updateBackup(completion: { _ in
                         DispatchQueue.main.async {
@@ -318,32 +100,13 @@ class BackupModalViewController: UIViewController {
                         }
                     }
                 }
-//            } else {
-//                Alerts.showBackupErrorWifi(controller: self)
-//            }
-//        }
     }
     
     lazy var loadingAlert = LoadingAlert(in: self)
     
-//    func isConnectedToWiFi(completion: @escaping (Bool) -> Void) {
-//        let monitor = NWPathMonitor()
-//        
-//        monitor.pathUpdateHandler = { path in
-//            if path.status == .satisfied && path.usesInterfaceType(.wifi) {
-//                completion(true)
-//            } else {
-//                completion(false)
-//            }
-//        }
-//        
-//        let queue = DispatchQueue(label: "NetworkMonitor")
-//        monitor.start(queue: queue)
-//    }
-    
     lazy var contentStackView: UIStackView = {
         let spacer = UIView()
-        let stackView = UIStackView(arrangedSubviews: [backupStatus, restoreBackup, updateBackup, loginButton, createAccountButton, spacer])
+        let stackView = UIStackView(arrangedSubviews: [backupStatusView, restoreBackup, updateBackup, loginView])
         stackView.axis = .vertical
         stackView.spacing = 1
         return stackView
@@ -358,25 +121,23 @@ class BackupModalViewController: UIViewController {
     }()
     
     let maxDimmedAlpha: CGFloat = 0.6
-    lazy var dimmedView: UIView = {
+    lazy var backgroundView: UIView = {
         let view = UIView()
         view.backgroundColor = .black
         view.alpha = maxDimmedAlpha
         return view
     }()
     
-    // Constants
     let defaultHeight: CGFloat = 530
     var currentContainerHeight: CGFloat = 460
     
-    // Dynamic container constraint
     var containerViewHeightConstraint: Constraint?
     var containerViewBottomConstraint: Constraint?
     
     init(backupIsActivated: Bool, delegate: BackupModalViewControllerDelegate) {
         super.init(nibName: nil, bundle: nil)
         self.delegate = delegate
-        switchControl.isOn = Defaults.getBool(.iCloudEnabled)
+        backupStatusView.switchControl.isOn = Defaults.getBool(.iCloudEnabled)
     }
     
     required init?(coder: NSCoder) {
@@ -388,11 +149,9 @@ class BackupModalViewController: UIViewController {
         setupView()
         setupConstraints()
         
-        // Adiciona um gesto de tap para fechar o modal
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleCloseAction))
-        dimmedView.addGestureRecognizer(tapGesture)
+        backgroundView.addGestureRecognizer(tapGesture)
         
-        // Adiciona um gesto de swipe para baixo
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.handleSwipeDown))
         swipeDown.direction = .down
         self.view.addGestureRecognizer(swipeDown)
@@ -412,24 +171,17 @@ class BackupModalViewController: UIViewController {
     }
     
     func setupConstraints() {
-        view.addSubview(dimmedView)
+        view.addSubview(backgroundView)
         view.addSubview(containerView)
         
-        containerView.addSubview(modalTitleView)
-        containerView.addSubview(modalSubtitleView)
+        containerView.addSubview(backupHeaderView)
         
-        modalTitleView.snp.makeConstraints { make in
+        backupHeaderView.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview().offset(8)
-            make.height.equalTo(44)  // Altura da barra de título
+            make.height.equalTo(80)
         }
         
-        modalSubtitleView.snp.makeConstraints { make in
-            make.top.equalTo(modalTitleView.snp.bottom)
-            make.leading.equalToSuperview().offset(16)
-            make.trailing.equalToSuperview().inset(16)
-        }
-        
-        dimmedView.snp.makeConstraints { make in
+        backgroundView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
         
@@ -441,34 +193,9 @@ class BackupModalViewController: UIViewController {
         
         containerView.addSubview(contentStackView)
         contentStackView.snp.makeConstraints { make in
-            make.top.equalTo(modalSubtitleView.snp.bottom).offset(24)
+            make.top.equalTo(backupHeaderView.snp.bottom).offset(24)
             make.bottom.equalTo(containerView.snp.bottom).offset(-20)
             make.leading.trailing.equalTo(containerView)
-        }
-        
-        containerView.addSubview(loginTitle)
-        containerView.addSubview(loginButton)
-        containerView.addSubview(createAccountTitle)
-        containerView.addSubview(createAccountButton)
-        
-        loginTitle.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.bottom.equalTo(loginButton.snp.top).inset(-12)
-        }
-        
-        loginButton.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.bottom.equalTo(createAccountTitle.snp.top).inset(-24)
-        }
-        
-        createAccountTitle.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.bottom.equalTo(createAccountButton.snp.top).inset(-12)
-        }
-        
-        createAccountButton.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(24)
-            make.bottom.equalToSuperview().inset(24)
         }
         
         containerViewHeightConstraint?.activate()
@@ -496,9 +223,9 @@ class BackupModalViewController: UIViewController {
     }
     
     func animateDismissView() {
-        dimmedView.alpha = maxDimmedAlpha
+        backgroundView.alpha = maxDimmedAlpha
         UIView.animate(withDuration: 0.4) {
-            self.dimmedView.alpha = 0
+            self.backgroundView.alpha = 0
         } completion: { _ in
             self.dismiss(animated: false)
         }
---
+++ Calculator++/Calculator Notes/Scenes/Backup/Views/BackupHeaderView.swift
@@ -0,0 +1,296 @@
+import UIKit
+import SnapKit
+import Network
+import Photos
+import AssetsPickerViewController
+import DTPhotoViewerController
+import CoreData
+import NYTPhotoViewer
+import ImageViewer
+import StoreKit
+import GoogleMobileAds
+import SceneKit
+import simd
+import Photos
+import StoreKit
+import Foundation
+import AVFoundation
+import AVKit
+import CloudKit
+import GoogleSignIn
+import UIKit
+import SnapKit
+
+class BackupHeaderView: UIView {
+    lazy var modalTitleView: UIView = {
+        let label = UILabel()
+        label.text = Text.backupSettings.localized()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    lazy var modalSubtitleView: UILabel = {
+        let label = UILabel()
+        label.text = Text.backupNavigationSubtitle.localized()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        addSubview(modalTitleView)
+        addSubview(modalSubtitleView)
+    }
+    
+    private func setupConstraints() {
+        modalTitleView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        modalSubtitleView.snp.makeConstraints { make in
+            make.top.equalTo(modalTitleView.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
+class BackupLoginView: UIView {
+    let manager = AuthManager()
+    
+    lazy var loginTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Já possui uma conta?"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login With Google", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGray6
+        
+        let googleLogo = UIImage(named: "google_logo")
+        button.setImage(googleLogo, for: .normal)
+        
+        button.imageView?.contentMode = .scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
+        
+        button.layer.cornerRadius = 25
+        button.layer.masksToBounds = true
+        
+        button.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func loginTapped() {
+        GIDSignIn.sharedInstance.signIn(withPresenting: controller) { signInResult, error in
+            guard let result = signInResult else {
+                if (error as? NSError)?.code == -5 {
+                    print("User cancelled")
+                } else {
+                    Alerts.showAlert(title: "Error", text: "\(error?.localizedDescription ?? "Something went wrong")\n\nSe você ainda não possui uma conta, selecione 'Sign up with google'", controller: self.controller)
+                }
+                return
+            }
+            
+            if let googleID = result.user.userID, let email = result.user.profile?.email {
+                self.manager.signInWithEmail(withEmail: email, password: googleID) { error in
+                    if let error = error {
+                        Alerts.showAlert(title: "Error", text: "\(error.localizedDescription)\n\nSe Você ainda não possui uma conta, selecione 'Sign up with google'", controller: self.controller)
+                    } else {
+                        Alerts.showAlert(title: "Login efetuado com sucesso!", text: "Suas fotos serão sincronizadas sempre que adicionar novas fotos ou clicar no botão 'atualizar backup'", controller: self.controller)
+                    }
+                }
+            } else {
+                Alerts.showAlert(title: "Error", text: "Something went wrong.", controller: self.controller)
+            }
+        }
+    }
+
+    lazy var createAccountTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Ainda não? Crie agora"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    lazy var createAccountButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign Up With Google", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGray6
+        
+        let googleLogo = UIImage(named: "google_logo")
+        button.setImage(googleLogo, for: .normal)
+        
+        button.imageView?.contentMode = .scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
+        
+        button.layer.cornerRadius = 25
+        button.layer.masksToBounds = true
+        
+        button.addTarget(self, action: #selector(createAccountTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func createAccountTapped() {
+        GIDSignIn.sharedInstance.signIn(withPresenting: controller) { signInResult, error in
+            guard let result = signInResult else {
+                if (error as? NSError)?.code == -5 {
+                    print("User cancelled")
+                } else {
+                    Alerts.showAlert(title: "Error", text: error?.localizedDescription ?? "Something went wrong", controller: self.controller)
+                }
+                return
+            }
+            
+            if let googleID = result.user.userID, let email = result.user.profile?.email {
+                self.manager.createAccount(withEmail: email, password: googleID) { error in
+                    if let error = error {
+                        Alerts.showAlert(title: "Error", text: error.localizedDescription, controller: self.controller)
+                    } else {
+                        Alerts.showAlert(title: "Login efetuado com sucesso!", text: "Suas fotos serão sincronizadas sempre que adicionar novas fotos ou clicar no botão 'atualizar backup'", controller: self.controller)
+                    }
+                }
+            } else {
+                Alerts.showAlert(title: "Error", text: "Something went wrong.", controller: self.controller)
+            }
+        }
+    }
+    
+    let controller: UIViewController
+    
+    init(controller: UIViewController) {
+        self.controller = controller
+        super.init(frame: .zero)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        addSubview(loginTitle)
+        addSubview(loginButton)
+        addSubview(createAccountTitle)
+        addSubview(createAccountButton)
+    }
+    
+    private func setupConstraints() {
+        loginTitle.snp.makeConstraints { make in
+//            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(loginTitle.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+        
+        createAccountTitle.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(24)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        createAccountButton.snp.makeConstraints { make in
+            make.top.equalTo(createAccountTitle.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
+class BackupStatusView: UIView {
+    var delegate: BackupModalViewControllerDelegate?
+    
+    lazy var switchControl: UISwitch = {
+        let switchControl = UISwitch()
+        switchControl.addTarget(self, action: #selector(switchValueChanged(_:)), for: .valueChanged)
+        return switchControl
+    }()
+    
+    lazy var backupStatus: UIView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 8
+        
+        let leftLabel = UILabel()
+        leftLabel.text = Text.backupStatus.localized()
+        leftLabel.font = UIFont.systemFont(ofSize: 14)
+        
+        stackView.addArrangedSubview(leftLabel)
+        stackView.addArrangedSubview(switchControl)
+        
+        let backupStatusView = UIView()
+        backupStatusView.backgroundColor = .systemGray5
+        backupStatusView.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview().inset(8)
+            make.leading.equalToSuperview().inset(16)
+        }
+        
+        backupStatusView.snp.makeConstraints { make in
+            make.height.equalTo(50)
+        }
+        
+        return backupStatusView
+    }()
+    
+    @objc func switchValueChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            Defaults.setBool(.iCloudEnabled, true)
+            self.delegate?.enableBackupToggled(status: true)
+        } else {
+            Defaults.setBool(.iCloudEnabled, false)
+            delegate?.enableBackupToggled(status: false)
+        }
+    }
+    
+    init(delegate: BackupModalViewControllerDelegate?) {
+        self.delegate = delegate
+        super.init(frame: .zero)
+        self.addSubview(backupStatus)
+        backupStatus.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
